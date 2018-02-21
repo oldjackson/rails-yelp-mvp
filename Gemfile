ba0f617f8ed829aba02db1cbf7a9a161f79bcd6f
Lorenzo Fermi <lorenzofermi@gmail.com>
2018-02-21 16:49:09 +0000

    Yelp completed (working skeleton)

--- Gemfile
+++ Gemfile
@@ -32,6 +32,11 @@ gem 'jbuilder', '~> 2.5'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+gem 'bootstrap-sass'
+gem 'font-awesome-sass'
+gem 'simple_form'
+gem 'autoprefixer-rails'
+gem 'jquery-rails' # Add this line if you use Rails 5.1
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
@@ -53,6 +58,7 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'faker'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+names = ["Pita Express Kitchen Inc.","Nick's Seabreeze Inn","Tuckahoe H.s.","El Coqui Family Rest.","Club Purgatory"]
+categories = ["chinese", "italian", "japanese", "french", "belgian"]
+
+names.each do |name|
+  reviews = []
+  rand(5..10).times do
+    reviews << Review.new(
+      content: Faker::Lorem.sentence,
+      rating: (0..5).to_a.sample
+    )
+  end
+  restaurant = Restaurant.new(
+    name: name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city} - #{Faker::Address.country}",
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: categories.sample,
+    reviews: reviews
+  )
+  restaurant.save!
+end
